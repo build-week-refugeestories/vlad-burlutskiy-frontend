255723a26b71d8a2a03cfd95f575135eeae878fd
Vladislav Burlutskiy <vladislavburlutskiy@MacBook-Pro.local>
2019-07-29 23:40:55 -0400

    styling main content on about us page

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -1,7 +1,7 @@
 
 
 
-    document.body.style.background = "url('front-end/images/sky3.jpg')";
+    document.body.style.background = "url('front-end/images/everyone_is_welcome.jpg')";
 
 //fixed Navigation bar 
     const nav = document.querySelector('#main');
