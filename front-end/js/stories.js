1bceaa2e177b5319b2d8f43987f20c2dc4dc31ae
Vladislav Burlutskiy <vladislavburlutskiy@MacBook-Pro.local>
2019-07-31 18:25:31 -0400

    home page, about us, stories page completed

--- front-end/js/stories.js
+++ front-end/js/stories.js
@@ -16,4 +16,17 @@
 
     window.addEventListener('scroll', fixNav);
 
+
     document.body.style.background = "url('front-end/images/seaM.jpg')";
+
+    // Hiding and showing the full story
+    document.querySelectorAll('.story-item p').forEach(story => {
+        const fullStory = story.innerText;
+        if(window.innerWidth < 2800){
+        const shortStory = story.innerText.slice(0,1255) + '...(click for full story)';
+        story.innerText = shortStory;
+        story.addEventListener('click', p => p.path[0].innerText = fullStory);
+        } else {
+            story.innerText = fullStory;
+        }
+        });
